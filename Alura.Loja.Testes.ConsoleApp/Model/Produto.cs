2a1d98ebee78124d955c7f6f355bdc2f93cff087
bruno-synctech <bruno@synctech.com.br>
2018-03-15 16:46:12 -0300

    first commit

---
+++ Alura.Loja.Testes.ConsoleApp/Model/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp {
+
+    internal class Produto {
+
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double Preco { get; internal set; }
+
+        public Produto() {
+
+        }
+
+        public Produto(int Id, string Nome, string Categoria, double Preco) {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Categoria = Categoria;
+            this.Preco = Preco;
+        }
+
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Alura.Loja.Testes.ConsoleApp {
+
+    class Program {
+
+        static void Main(string[] args) {
+            //  Call methods
+        }
+
+        private static void Add() {
+            var produto = new Produto(0, "Harry Potter e a Ordem da Fênix", "Livros", 19.89);
+            var produtoDAO = new ProdutoDAO();
+            produtoDAO.Save(produto);
+        }
+
+        private static void Update() {
+            var produto = new Produto(1, "Harry Potter e a Ordem da Fênix", "Livros", 29.90);
+            var produtoDAO = new ProdutoDAO();
+            produtoDAO.Update(produto);
+        }
+
+        private static void Remove() {
+            var produto = new Produto(1, "Harry Potter e a Ordem da Fênix", "Livros", 29.90);
+            var produtoDAO = new ProdutoDAO();
+            produtoDAO.Remove(produto);
+        }
+
+        private static void List() {
+            var produtoDAO = new ProdutoDAO();
+            List<Produto> produtos = produtoDAO.FindAll();
+            foreach(Produto produto in produtos) {
+                Debug.WriteLine(produto.Nome);
+                Debug.WriteLine(produto.Categoria);
+                Debug.WriteLine(produto.Preco);
+            }
+        }
+    }
+}
